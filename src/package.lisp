97df1f75d5b8309daba6b3a010641ed83e2eebcc
James Fleming <james@electronic-quill.net>
2018-04-22 16:47:08 +0200

    Export the superclasses

--- src/package.lisp
+++ src/package.lisp
@@ -2,15 +2,18 @@
   (:use
     #:cl)
   (:export
+    ip-address
     ipv4-address
     ipv6-address
     ip-version
     as-string
     as-integer
     as-cidr
+    ip-interface
     ipv4-interface
     ipv6-interface
     prefix-length
+    ip-subnet
     ipv4-subnet
     ipv6-subnet
     address
